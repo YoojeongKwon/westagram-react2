7c3e1f89daf62dd9eee80a770182e27d2957be32
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-02-21 21:57:11 +0900

    ADD: Router & Sass

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "./Login.scss";
+import "../../styles/common.scss";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate("/main");
+  };
+
+  return (
+    <main className="totalWrap">
+      <h1 className="westagram">Westagram</h1>
+      <input
+        className="id"
+        type="text"
+        placeholder="전화번호, 사용자 이름 또는 이메일"
+      />
+      <input className="password" type="password" placeholder="비밀번호" />
+      <button className="loginButton" onClick={goToMain} disabled>
+        로그인
+      </button>
+      <a href="#none" className="forgetText">
+        비밀번호를 잊으셨나요?
+      </a>
+    </main>
+  );
+};
+
+export default Login;
+
+// // "use strict" => 자바스크립트는 동적 언어이므로 에러 방지를 위해 써준다
+// // querySelector 는 노드를 가져온다
+
+// const id = document.querySelector(".id");
+// const pw = document.querySelector(".password");
+// const button = document.querySelector(".loginButton");
+
+// // 선언식 함수, 표현식 함수
+// const activeButton = () => {
+//   if (id.value && pw.value) {
+//     // button.setAttribute("disabled", false) 는 적용되지 않는다.
+//     button.removeAttribute("disabled");
+//     button.style.backgroundColor = "#389ed3";
+//   } else {
+//     // else 조건이 없으면 버튼이 활성화된 후 id 또는 pw를 지웠을 때 다시 비활성화 상태로 돌아가지 않는다.
+//     button.setAttribute("disabled", true);
+//     button.style.backgroundColor = "#c4e1fb";
+//   }
+// };
+
+// id.addEventListener("keyup", (event) => {
+//   activeButton();
+// });
+
+// pw.addEventListener("keyup", (event) => {
+//   activeButton();
+// });
+
+// button.addEventListener("click", (e) => {
+//   // 아이디 @ 포함, 비밀번호 5자리 이상
+//   // 조건 불충족시 pw input 아래에 빨간 글자 생성
+//   if (id.value.indexOf("@") === -1) {
+//     alert("아이디를 올바르게 입력하세요.");
+//   }
+//   if (pw.value.length < 5) {
+//     alert("비밀번호는 5자리 이상이어야 합니다.");
+//   }
+// });
+
+// // location.replace("주소") => 주소로 이동. 로그인 성공했을 떄 아래에 넣어주면 다음 페이지로 넘어갈 수 있다.
+// // inputs.addEventListener("input", 함수 이름) => 이벤트 위임. 부모(변수 inputs)의 이벤트가 자식(input 태그)에게도 동일하게 적용된다.
+// // vs 이벤트 버블링
+// // 함수를 분리해서 작성하는 습관 => 유지, 보수를 위해 => 하나의 함수는 하나의 동작(하나의 리턴)만 실행하는 것이 베스트
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,425 @@
+import React from "react";
+import "./Main.scss";
+import "../../styles/common.scss";
+import { useNavigate } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <React.Fragment>
+      <nav className="upperNav">
+        <div className="westaWrap">
+          <img
+            id="instagramLogo"
+            src="/images/instagram.png"
+            alt="인스타그램 로고"
+          />
+          <div className="westagram">Westagram</div>
+        </div>
+        <input className="search" type="text" placeholder="검색" />
+        <div>
+          <button className="upperIconsButton">
+            <img
+              className="upperIcons"
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+              alt="탐색"
+            />
+          </button>
+          <button className="upperIconsButton">
+            <img
+              className="upperIcons"
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+              alt="하트"
+            />
+          </button>
+          <button className="upperIconsButton" id="dropButton">
+            <img
+              className="upperIcons"
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+              alt="프로필"
+            />
+          </button>
+        </div>
+        <div id="dropBox">
+          <a href="#none">
+            <img src=".images/save-instagram.png" alt="프로필" />
+            프로필
+          </a>
+          <a href="#none">
+            <img src="/images/save-instagram.png" alt="저장" />
+            저장됨
+          </a>
+          <a id="setting" href="#none">
+            <img src="/images/save-instagram.png" alt="설정" />
+            설정
+          </a>
+          <a id="log_out" href="#none">
+            로그아웃
+          </a>
+        </div>
+      </nav>
+      <div className="mainWrap">
+        <div className="leftSection">
+          <div className="leftSectionArticle">
+            <nav className="profileNav">
+              <span className="profileNavInner1">
+                <img
+                  className="profileImg"
+                  src="/images/user.png"
+                  alt="프로필"
+                />
+                <a href="#none" className="profileName">
+                  canon_mj
+                </a>
+              </span>
+              <span className="profileNavInner2">...</span>
+            </nav>
+            <img
+              className="uploadImg"
+              src="/images/chad-montano-MqT0asuoIcU-unsplash.jpg"
+              alt="피자"
+            />
+            <div className="profileIconWrap">
+              <button className="profileHeart">
+                <img
+                  className="profileHeart"
+                  src="/images/heart.png"
+                  alt="하트"
+                />
+              </button>
+              <button className="profileComment">
+                <img
+                  className="profileComment"
+                  src="/images/speech-bubble.png"
+                  alt="댓글"
+                />
+              </button>
+              <button className="profileUproad">
+                <img
+                  className="profileUproad"
+                  src="/images/upload.png"
+                  alt="업로드"
+                />
+              </button>
+              <button className="profileSave">
+                <img
+                  className="profileSave"
+                  src="/images/save-instagram.png"
+                  alt="저장"
+                />
+              </button>
+            </div>
+            <div className="likeWrap">
+              <a href="#none" className="likeId">
+                aineworld
+              </a>
+              님<span className="likeCount">외 10명</span>이 좋아합니다.
+            </div>
+            <div className="newComment">
+              {/* <div class="commentTotalWrap">
+                    <a href="#" class="commentId">neceosecius</a>
+                    <span class="comment">댓글 예시</span>              
+                    <button class="commentLikeButton"><img class="commentLike" src="img/heart.png" alt="하트"/><button>
+                    <button class="deleteComment">x</button>
+                  </div> */}
+            </div>
+            <div className="commentTime">42분 전</div>
+            <div className="addcommentWrap">
+              <input
+                className="addComment"
+                type="text"
+                placeholder="댓글 달기..."
+              />
+              <button className="addCommentButton">게시</button>
+            </div>
+          </div>
+        </div>
+        <div className="rightSection">
+          <div className="userProfileWrap">
+            <img
+              className="userProfileImg"
+              src="/images/account.png"
+              alt="프로필"
+            />
+            <div className="userIdWrap">
+              <div className="userId">wecode_bootcamp</div>
+              <div className="userCompany">WeCode | 위코드</div>
+            </div>
+          </div>
+          <div className="storyTotalWrap">
+            <div className="storyWrap">
+              <span className="rightSectionTitle">스토리</span>
+              <button className="seeAll">모두 보기</button>
+            </div>
+            <ul>
+              <li className="storyProfileWrap">
+                <img
+                  className="storyProfileImg"
+                  src="/images/karl-magnuson-85J99sGggnw-unsplash.jpg"
+                  alt="sunset"
+                />
+                <div className="storyIdWrap">
+                  <a href="#none" className="storyId">
+                    _yum_s
+                  </a>
+                  <div className="storyTime">16분 전</div>
+                </div>
+              </li>
+              <li className="storyProfileWrap">
+                <img
+                  className="storyProfileImg"
+                  src="/images/samsung-uk-no101tswX7I-unsplash.jpg"
+                  alt="대화하는 두 여성"
+                />
+                <div className="storyIdWrap">
+                  <a href="#none" className="storyId">
+                    drink_eat_drink
+                  </a>
+                  <div className="storyTime">3시간 전</div>
+                </div>
+              </li>
+              <li className="storyProfileWrap">
+                <img
+                  className="storyProfileImg"
+                  src="/images/karl-magnuson-85J99sGggnw-unsplash.jpg"
+                  alt="sunset"
+                />
+                <div className="storyIdWrap">
+                  <a href="#none" className="storyId">
+                    hyukyc
+                  </a>
+                  <div className="storyTime">20시간 전</div>
+                </div>
+              </li>
+              <li className="storyProfileWrap">
+                <img
+                  className="storyProfileImg"
+                  src="/images/samsung-uk-no101tswX7I-unsplash.jpg"
+                  alt="대화하는 두 여성"
+                />
+                <div className="storyIdWrap">
+                  <a href="#none" className="storyId">
+                    jminkeek
+                  </a>
+                  <div className="storyTime">22시간 전</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="recommendTotalWrap">
+            <div className="recommendWrap">
+              <span className="rightSectionTitle">회원님을 위한 추천</span>
+              <button className="seeAll">모두 보기</button>
+            </div>
+            <ul>
+              <li className="recommendProfileWrap">
+                <div className="recommendImgWrap">
+                  <img
+                    className="recommendProfileImg"
+                    src="/images/samsung-uk-no101tswX7I-unsplash.jpg"
+                    alt="대화하는 두 여성"
+                  />
+                  <div className="recommendIdWrap">
+                    <a href="#none" className="recommendId">
+                      joaaaaahye
+                    </a>
+                    <div className="recommendFollower">
+                      _legend_a님 외 2명이...
+                    </div>
+                  </div>
+                </div>
+                <button className="follow">팔로우</button>
+              </li>
+              <li className="recommendProfileWrap">
+                <div className="recommendImgWrap">
+                  <img
+                    className="recommendProfileImg"
+                    src="/images/karl-magnuson-85J99sGggnw-unsplash.jpg"
+                    alt="sunset"
+                  />
+                  <div className="recommendIdWrap">
+                    <a href="#none">rampart81</a>
+                    <div className="recommendFollower">
+                      ringo.in.seoul님 외 12...
+                    </div>
+                  </div>
+                </div>
+                <button className="follow">팔로우</button>
+              </li>
+              <li className="recommendProfileWrap">
+                <div className="recommendImgWrap">
+                  <img
+                    className="recommendProfileImg"
+                    src="/images/samsung-uk-no101tswX7I-unsplash.jpg"
+                    alt="대화하는 두 여성"
+                  />
+                  <div className="recommendIdWrap">
+                    <a href="#none">shawnjjoo</a>
+                    <div className="recommendFollower">
+                      jimmylee1220님 외 1...
+                    </div>
+                  </div>
+                </div>
+                <button className="follow">팔로우</button>
+              </li>
+            </ul>
+          </div>
+          <p className="etcWrap">
+            <a className="etc" href="#">
+              instagram 정보
+            </a>
+            ・
+            <a className="etc" href="#">
+              지원
+            </a>
+            ・
+            <a className="etc" href="#">
+              홍보 센터
+            </a>
+            ・
+            <a className="etc" href="#">
+              API
+            </a>
+            <a className="etc" href="#">
+              채용
+            </a>
+            ・
+            <a className="etc" href="#">
+              정보
+            </a>
+            ・
+            <a className="etc" href="#">
+              개인정보처리방침
+            </a>
+            ・
+            <a className="etc" href="#">
+              약관
+            </a>
+            ・
+            <a className="etc" href="#">
+              디렉터리
+            </a>
+            ・
+            <a className="etc" href="#">
+              프로필
+            </a>
+            ・
+            <a className="etc" href="#">
+              해시태그
+            </a>
+            ・
+            <a className="etc" href="#">
+              언어
+            </a>
+          </p>
+          <p className="etc">© 2023 WESTAGRAM</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Main;
+
+// const addButton = document.querySelector(".addCommentButton");
+
+// const newComment = document.querySelector(".newComment");
+// const addComment = document.querySelector(".addComment");
+
+// // 게시 버튼 누르면 댓글 추가
+// // const newComment = `<span>아이디</span><span>${변수(댓글 내용)}</span>`
+// // newCommentList.innerHTML = newComment
+// // => getElement로 각각의 span 생성해서 value를 appendChild 등등을 할 필요가 없어짐!
+// addButton.addEventListener("click", (event) => {
+//   createComment();
+// });
+
+// const createComment = () => {
+//   if (addComment.value === "") {
+//     return;
+//   }
+
+//   const commentTotalWrap = document.createElement("div");
+//   commentTotalWrap.classNameName = "commentTotalWrap";
+
+//   const commentId = document.createElement("a");
+//   commentId.href = "#";
+//   commentId.classNameName = "commentId";
+//   commentId.innerText = "user Id";
+//   commentId.style.marginRight = "8px";
+
+//   const comment = document.createElement("span");
+//   comment.classNameName = "comment";
+//   comment.innerText = addComment.value;
+
+//   const commentLike = document.createElement("button");
+//   const heartImg = document.createElement("img");
+//   comment.style.width = heartImg.width;
+//   heartImg.classNameName = "commentLike";
+//   heartImg.src = "img/heart.png";
+//   heartImg.alt = "하트";
+
+//   const deleteComment = document.createElement("button");
+//   deleteComment.classNameName = "deleteComment";
+//   deleteComment.innerText = "x";
+
+//   commentLike.appendChild(heartImg);
+
+//   commentTotalWrap.appendChild(commentId);
+//   commentTotalWrap.appendChild(comment);
+//   commentTotalWrap.appendChild(commentLike);
+//   commentTotalWrap.appendChild(deleteComment);
+
+//   newComment.appendChild(commentTotalWrap);
+
+//   addComment.value = "";
+
+//   if (addComment.value === "") {
+//     addComment.focus();
+//   }
+
+//   // x 버튼 누르면 댓글 삭제
+//   deleteComment.addEventListener("click", (e) => {
+//     commentTotalWrap.remove();
+//   });
+// };
+
+// addComment.addEventListener("keypress", (event) => {
+//   if (event.key === "Enter") {
+//     return createComment();
+//   }
+// });
+
+// const profileHeart = document.getElementsByclassNameName("profileHeart")[1];
+// // 하트 누르면 빨간 하트
+// profileHeart.addEventListener("click", (e) => {
+//   profileHeart.src = "img/redheart.png";
+
+//   // if (profileHeart.src === "img/redheart.png") {
+//   //   profileHeart.src = "img/heart.png";
+//   // }
+// });
+
+// const profileSave = document.getElementsByclassNameName("profileSave")[1];
+// // 저장 누르면 채워진 아이콘
+// profileSave.addEventListener("click", (e) => {
+//   profileSave.src = "img/bookmark.png";
+
+//   // if (profileSave.src === "img/bookmark.png") {
+//   //   profileSave.src = "img/save-instagram.png";
+//   // }
+// });
+
+// const dropButton = document.getElementById("dropButton");
+// const dropBox = document.getElementById("dropBox");
+
+// // 드롭버튼 클릭하면 dropbox 보이도록
+// dropButton.addEventListener("click", (e) => {
+//   dropBox.style.display = "block";
+// });
+
+// // 전역에 클릭이벤트 주어서 드롭박스 사라지게
+// const removeDropBox = document.querySelector(".mainWrap");
+
+// removeDropBox.addEventListener("click", (e) => {
+//   dropBox.style.display = "none";
+// });
